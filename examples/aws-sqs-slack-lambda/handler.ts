897b8ada3cef8d3f02ff712bb9b029fee62cec7e
Renato Golia <renato@golia.tech>
2026-09-03 11:53:14 +0200

    Simplify SQS Slack Lambda handler

--- examples/aws-sqs-slack-lambda/handler.ts
+++ examples/aws-sqs-slack-lambda/handler.ts
@@ -1,32 +1,11 @@
-import type { Context } from "@hooksmith/core";
-import { createRuntime } from "@hooksmith/runtime";
 import { fromSqs } from "@hooksmith/aws/sqs";
 import { createProcessor } from "@hooksmith/aws-lambda";
 import { createHandler } from "@hooksmith/aws-lambda/sqs";
-import config, {
-  type QueueItem,
-  type QueueItemEvent,
-} from "./hooksmith.config.ts";
+import { createRuntime } from "@hooksmith/runtime";
+import config from "./hooksmith.config.ts";
 
-const context: Context = {
-  log: {
-    debug(message, ...args) {
-      console.debug(message, ...args);
-    },
-    info(message, ...args) {
-      console.info(message, ...args);
-    },
-    warn(message, ...args) {
-      console.warn(message, ...args);
-    },
-    error(message, ...args) {
-      console.error(message, ...args);
-    },
-  },
-};
-
-const runtime = createRuntime<QueueItemEvent>(config, context);
-const processor = createProcessor<QueueItem>(runtime);
+const context = { log: console };
+const processor = createProcessor(createRuntime(config, context));
 
 export const handler = createHandler(
   fromSqs,
