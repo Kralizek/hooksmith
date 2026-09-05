57cc1dfcfbded93db508a41d07c82428383c66e4
Renato Golia <renato@golia.tech>
2026-09-05 13:08:35 +0200

    Export console log writer

--- packages/runtime/mod.ts
+++ packages/runtime/mod.ts
@@ -1,5 +1,6 @@
 export { hydrateEvent } from "./hydrate.ts";
 export {
+  createConsoleLogWriter,
   createLoggerFactory,
   nullLoggerFactory,
   renderLogTemplate,
@@ -9,6 +10,7 @@ export type {
   LogLevel,
   LogLevelFilter,
   LogRecord,
+  LogWriter,
 } from "./logging.ts";
 export { createRuntime } from "./runtime.ts";
 export type {
