5684e51735cedd4681e154a964e98968a1847dd6
Renato Golia <renato@golia.tech>
2026-09-05 18:15:12 +0200

    Use runtime null logger in runtime tests

--- packages/runtime/mod_test.ts
+++ packages/runtime/mod_test.ts
@@ -1,12 +1,6 @@
 import { assertEquals, assertRejects, assertThrows } from "@std/assert";
-import {
-  type Config,
-  type Context,
-  type Event,
-  type Listener,
-  nullLoggerFactory,
-} from "@hooksmith/core";
-import { createRuntime, hydrateEvent } from "./mod.ts";
+import type { Config, Context, Event, Listener } from "@hooksmith/core";
+import { createRuntime, hydrateEvent, nullLoggerFactory } from "./mod.ts";
 
 const context: Context = {
   logger: nullLoggerFactory,
