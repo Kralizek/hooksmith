fe9c58b252054833a0f42bf648afe0d7d4f1dd32
Renato Golia <renato@golia.tech>
2026-09-05 15:11:16 +0200

    Export shared telemetry contract

--- packages/core/mod.ts
+++ packages/core/mod.ts
@@ -122,3 +122,17 @@ export interface Config<TEvent extends Event = Event> {
   routes: Route<TEvent>[];
   fallback?: Listener<TEvent>[];
 }
+
+export {
+  getTelemetry,
+  setTelemetry,
+} from "./telemetry.ts";
+export type {
+  Telemetry,
+  TelemetryAttributes,
+  TelemetryAttributeValue,
+  TelemetryCounter,
+  TelemetryHistogram,
+  TelemetryInstrumentOptions,
+  TelemetrySpan,
+} from "./telemetry.ts";
